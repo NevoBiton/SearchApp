018ec04cfbb67aa28d5ae6de976ff63d440e1ab4
nevobiton <nevo@trustedpartners.com>
2025-03-01 22:18:55 +0200

    margin

--- searchFrontend/src/costumComponents/SearchSkeletons.tsx
+++ searchFrontend/src/costumComponents/SearchSkeletons.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const SearchSkeletons = () => {
     return (
-        <div className="space-y-4">
+        <div className="space-y-4 mt-4">
             {[...Array(5)].map((_, index) => (
                 <div key={index} className="flex flex-col space-y-2 p-4 border rounded-md bg-card">
                     <Skeleton className="h-6 w-3/4" />
